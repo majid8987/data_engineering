63f97629c513236d6cde2185452efa7919e8d7da
majid8987 <majidalikhaja90@gmail.com>
2024-10-17 11:13:25 -0400

    added a new file

---
+++ src/lists.py
@@ -0,0 +1,11 @@
+l= [2,4,6,8,10,12,14,16]
+largest_number = max(l)
+smallest_number = min(l)
+sort_out = l.sort(reverse=True)
+print(l)
+
+print(largest_number)
+print(smallest_number)
+print(sort_out)
+print(sum(l))
+
