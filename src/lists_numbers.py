2f96f02d9a081a9f508c3c8b66ef32f9ff439129
majid8987 <majidalikhaja90@gmail.com>
2024-10-18 03:56:42 -0400

    did some solutions in list of numbers

---
+++ src/lists_numbers.py
@@ -0,0 +1,13 @@
+List_numbers= [3,1,4,1,5,9,2,6,5,1,3,2,8,9,5,4,7]
+List_numbers.append(22)
+List_numbers
+
+List_numbers.remove(7)
+List_numbers
+
+List_numbers.sort(reverse=True) 
+List_numbers
+
+List_numbers= [3,1,4,1,5,9,2,6,5,1,3,2,8,9,5,4,7]
+List_numbers.insert(2,2)
+List_numbers
