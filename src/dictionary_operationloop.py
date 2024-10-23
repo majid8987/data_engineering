421380e131e923e68ed42c822a95cbe97e613e02
majid8987 <majidalikhaja90@gmail.com>
2024-10-23 11:33:33 -0400

    added dictionary and operation looping statement

--- src/dictionary_operationloop.py
+++ src/dictionary_operationloop.py
@@ -12,13 +12,15 @@
    # "Bob": {"Math": 70, "English": 85, "Science": 75},
     #"Charlie": {"Math": 95, "English": 60, "Science": 85}
     
-  students = {
+    
+    
+students = {
       "Alice": {"Math": 80, "English": 75, "Science": 90},
     "Bob": {"Math": 70, "English": 85, "Science": 75},
     "Charlie": {"Math": 95, "English": 60, "Science": 85}
   }
 
- def cal_avg(students):
+def cal_avg(students):
     avg = {}
     for student, subjects in students.items():
         total = 0
